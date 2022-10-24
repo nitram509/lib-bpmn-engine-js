62d820a0e2918b736af09cb49813254799806f23
nitram509 <maki@bitkings.de>
2022-10-24 20:54:19 +0200

    add more docs and github links add gh-update action

--- static/bpmn-modeler.js
+++ static/bpmn-modeler.js
@@ -12,31 +12,14 @@ var bpmnModeler = new BpmnJS({
  * @param {String} bpmnXML diagram to display
  */
 async function openDiagram(bpmnXML) {
-
     // import diagram
     try {
-
         await bpmnModeler.importXML(bpmnXML);
-
         // access modeler components
         var canvas = bpmnModeler.get('canvas');
         var overlays = bpmnModeler.get('overlays');
-
-
         // zoom to fit full viewport
         canvas.zoom('fit-viewport');
-
-        // // attach an overlay to a node
-        // overlays.add('SCAN_OK', 'note', {
-        //     position: {
-        //         bottom: 0,
-        //         right: 0
-        //     },
-        //     html: '<div class="diagram-note">Mixed up the labels?</div>'
-        // });
-
-        // add marker
-        // canvas.addMarker('SCAN_OK', 'needs-discussion');
     } catch (err) {
         console.error('could not import BPMN 2.0 diagram', err);
     }
